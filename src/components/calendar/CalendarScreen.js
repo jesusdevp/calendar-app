2af04cc1d29797b83ba606bb6e67a725f86f3431
Jesus Ramirez <jesusdevelp@gmail.com>
2021-04-19 15:23:48 -0500

    React Big Calendar Config

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,10 +1,50 @@
 import React from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
 import { Navbar } from "../ui/Navbar";
+import { messages } from "../../helpers/calendar-messages-es";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import "moment/locale/es";
+
+moment.locale("es");
+
+const localizer = momentLocalizer(moment);
+
+const events = [
+  {
+    title: "Cumpleaños del jefe",
+    start: moment().toDate(), // New date
+    end: moment()
+      .add(2, "hours")
+      .toDate(),
+    bgcolor: "#fafafa",
+  },
+];
 
 export const CalendarScreen = () => {
+  const eventStylesGetter = (event, start, end, isSelected) => {
+    const style = {
+      backgroundColor: "#367cf7",
+      borderRadius: "0px",
+      opacity: 0.8,
+      display: "block",
+      color: "white",
+    };
+
+    return { style };
+  };
+
   return (
-    <div>
+    <div className="container-calendar">
       <Navbar />
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccesor="start"
+        endAccesor="end"
+        messages={messages}
+        eventPropGetter={eventStylesGetter}
+      />
     </div>
   );
 };
