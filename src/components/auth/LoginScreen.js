d1c591756d0f4559b80d9f5049dce8d3a4e968db
Jesus Ramirez <jesusdevelp@gmail.com>
2021-04-19 14:42:39 -0500

    AuthRouter component and Navbar

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,9 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Login.css";
 
 export const LoginScreen = () => {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="container login-container">
+      <div className="col-md-6 login-form-1">
+        <h3>Sign in</h3>
+        <form>
+          <div className="form-group">
+            <input type="text" className="form-control" placeholder="Email" />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Password"
+            />
+          </div>
+          <div className="form-group btn-container">
+            <input type="submit" className="btnSubmit" value="Login" />
+          </div>
+          <Link to="/auth/register">
+            <p>Create Account</p>
+          </Link>
+        </form>
+      </div>
     </div>
   );
 };
--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,9 +1,42 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Register.css";
 
 export const RegisterScreen = () => {
   return (
-    <div>
-      <h1>Register Screen</h1>
+    <div className="container register-container">
+      <div className="col-md-6 register-form">
+        <h3>Sign up</h3>
+        <form>
+          <div className="form-group">
+            <input type="text" className="form-control" placeholder="Name" />
+          </div>
+          <div className="form-group">
+            <input type="email" className="form-control" placeholder="Email" />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Contraseña"
+            />
+          </div>
+
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Repeat Password"
+            />
+          </div>
+          <div className="form-group btn-container">
+            <input type="submit" className="btnSubmit" value="Create Account" />
+          </div>
+          <Link to="/auth/login">
+            <p className="signup">Sign in</p>
+          </Link>
+        </form>
+      </div>
     </div>
   );
 };
